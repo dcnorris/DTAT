54626d165a243ba507c2c587cb4329e13fe87158
David Norris <david@UnusualSolutionsThatWork.com>
2018-01-07 11:44:31 -0800

    Apply -top.dose- to DS plot as well as OX

--- R/OXDSplot.R
+++ R/OXDSplot.R
@@ -1,11 +1,16 @@
-OXDSplot <- function(de, tox.pending=FALSE, ..., ox.width=0.7){
+OXDSplot <- function(de, tox.pending=FALSE, periods=max(de$period),
+                     top.dose=max(de$dose), ox.width=0.7){
   ylim <- range(de$dose) + c(-0.5, 0.5) # xyplot.survfit default is insufficient
-  ox <- OXplot(de, tox.pending, ...)
+  ox <- OXplot(de, tox.pending=tox.pending, periods=periods, top.dose=top.dose)
   if(tox.pending){
     de <- subset(de, period < max(de$period))
   }
+  if(top.dose < max(de$dose)){
+    top.dose <- max(de$dose)
+    warning(paste("Increasing top.dose to", top.dose, "to avoid lopping top off OXDSplot."))
+  }
   fit <- dose.survfit(de)
-  ds <- xyplot(fit, ylim)
+  ds <- xyplot(fit, ylim=0.5+c(0, top.dose))
   print(ox, position=c(0, 0, ox.width+0.02, 1), more=TRUE)
   print(ds, position=c(ox.width-0.02, 0, 1, 1), more=FALSE)
 }
