c0a750a7ce085440fd1c2dbb22c96bbc51a92697
David Norris <david@UnusualSolutionsThatWork.com>
2018-01-07 11:01:45 -0800

    Dwell on end-of-study results before anim start

--- R/OXDSanimate.R
+++ R/OXDSanimate.R
@@ -2,6 +2,9 @@
 OXDSanimate <- function(de){
   periods <- max(last(de)$period)
   top.dose <- max(last(de)$dose)
+  for(dwell in 1:3){ # dwell on end-of-study results before animation starts
+    OXDSplot(last(de))
+  }
   for(period in seq_along(de)){
     OXDSplot(de[[period]], tox.pending=TRUE, periods=periods, top.dose=top.dose)
     OXDSplot(de[[period]], tox.pending=FALSE, periods=periods, top.dose=top.dose)
