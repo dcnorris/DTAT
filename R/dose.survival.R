c1ee4ccecc531a1ce71170acab032b77631909a3
David Norris <david@UnusualSolutionsThatWork.com>
2018-01-07 22:28:33 -0800

    Add a TODO re need for 'plainer' summary

--- R/dose.survival.R
+++ R/dose.survival.R
@@ -2,6 +2,8 @@
 # using interval-censoring constructs a la type='interval2'
 dose.survival <- function(de){
   de <- de[,c('id','dose','dlt')]
+  # TODO: Use plainer code (like summary.default?) to avoid 'no visible binding'
+  #       complaints around max(dose) during package check.
   suppressMessages({
     L <- summarize(group_by(de[!de$dlt,], id), doseL=max(dose))
     suppressWarnings( # expect min() to yield Inf often below
