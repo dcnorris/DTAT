1fef5f68022dff29e592cb890d2348e7d9b19910
David Norris <david@UnusualSolutionsThatWork.com>
2019-01-27 08:56:32 -0500

    Dead comment removal

--- R/OXDSplot.R
+++ R/OXDSplot.R
@@ -24,8 +24,6 @@ OXDSplot <- function(de, tox.pending=FALSE, periods=max(de$period),
   ylim <- range(de$dose) + c(-0.5, 0.5) # xyplot.survfit default is insufficient
   ox <- OXplot(de, tox.pending=tox.pending, periods=periods, top.dose=top.dose)
   if(tox.pending){
-    # Next line causes a package-check NOTE re "global variable 'period'"
-    #de <- subset(de, period < max(de$period))
     de <- de[de$period < max(de$period),]
   }
   if(top.dose < max(de$dose)){
